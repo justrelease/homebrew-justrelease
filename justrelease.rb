8cd650b0f0c2c919836c8600ea7ce742f52cdd6d
Bilal Yasar <bilalyasar45@gmail.com>
2015-09-22 01:11:57 +0300

    Update justrelease.rb

--- justrelease.rb
+++ justrelease.rb
@@ -1,6 +1,6 @@
 class Justrelease < Formula
   homepage "https://github.com/justrelease"
-  url "https://github.com/justrelease/justrelease/releases/download/v1.0.0/justrelease-1.0.0.zip"
+  url "https://github.com/justrelease/justrelease/releases/download/v1.0.1/justrelease-1.0.1.zip"
   sha256 "46be6e3e9fa53e5c07f8a854de568f47ee64492989e6cc4d242cce0bf5544a8d"
 
   depends_on :java
