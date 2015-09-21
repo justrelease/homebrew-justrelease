32bf7eba5cadc806f65017ee4de838c60e8f04ef
Bilal Yasar <bilalyasar45@gmail.com>
2015-09-22 01:05:59 +0300

    Update justrelease.rb

--- justrelease.rb
+++ justrelease.rb
@@ -1,6 +1,4 @@
-require 'formula'
 class Justrelease < Formula
-  desc "CLI tool to release github projects."
   homepage "https://github.com/justrelease"
   url "https://github.com/justrelease/justrelease/releases/download/v1.0.0/justrelease-1.0.0.zip"
   sha256 "46be6e3e9fa53e5c07f8a854de568f47ee64492989e6cc4d242cce0bf5544a8d"
